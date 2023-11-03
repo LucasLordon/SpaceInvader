ce52bf786acc1fb1694f9b04e58bd3e5a063f6e8
LucasLordon <122774884+LucasLordon@users.noreply.github.com>
2023-11-03 22:22:22 +0100

    Edit Test and Rapport

--- ModelTests/AlienTests.cs
+++ ModelTests/AlienTests.cs
@@ -10,14 +10,24 @@ namespace Model.Tests
         [TestMethod()]
         public void moveTest()
         {
-            // Test le déplacement normal de l'alien
+            // Test le déplacement normal de l'alien (moveTestNormal)
             Alien alien1 = new Alien(0, 0, "alien.png", 30, 40, 5);
             alien1.move(100); // Supposez que la largeur maximale soit de 100
             Assert.AreEqual(5, alien1.posX);
 
-            // Test le déplacement lorsque l'alien atteint la limite
+
+        }
+        [TestMethod()]
+        public void moveTestExeption()
+        {
+            // Arrange
+            // Test le déplacement lorsque l'alien atteint la limite (moveTestExeption)
             Alien alien2 = new Alien(130, 0, "alien.png", 30, 40, 5);
+
+            //Act
             alien2.move(100); // Supposez que la largeur maximale soit de 100
+
+            //Assert
             // L'alien doit être réinitialisé à une nouvelle ligne et se déplacer vers la gauche
             Assert.AreEqual(-40, alien2.posX); // Nouvelle position X après le rebond
             Assert.AreEqual(50, alien2.posY); // Nouvelle position Y après le rebond
